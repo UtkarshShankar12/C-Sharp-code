f61d7c4bc4eb2fe729e627827cf7dd2442660b46
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-12 20:48:33 +0530

    Revamp first C# Code

---
+++ Using Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "Utkarsh";
+            int CharacterAge = 35;
+            Console.WriteLine("There once a man named "+CharacterName);
+            Console.WriteLine("he was " + CharacterAge + " years old");
+            Console.WriteLine("he really liked the name. "+CharacterName);
+            Console.WriteLine("But he didn't like beign "+CharacterAge);
+            Console.ReadLine();
+        }
+    }
+}
