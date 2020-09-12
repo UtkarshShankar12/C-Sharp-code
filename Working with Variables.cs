0ba726608f9309880a49a490827b72f42f14dfe2
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-12 20:44:57 +0530

    Second C# Code

---
+++ Working with Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = ("UVGameCompany" + "This is my company");
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Contains("company"));
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase.IndexOf("company"));
+            Console.WriteLine(phrase.Substring(9,3));
+            Console.ReadLine();
+        }
+    }
+}
