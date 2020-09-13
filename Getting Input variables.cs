44c68be9238c33630cb01cbe53adb3a8464fb47d
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-13 23:29:31 +0530

    Fourth C# Code

---
+++ Getting Input variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age );
+            Console.ReadLine();
+        }
+    }
+}
