a4662a1378200be8397cbc91a39df7805e64a492
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-13 23:23:06 +0530

    Third C# Code

---
+++ Working with Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            num++;
+            Console.WriteLine(num);
+            Console.WriteLine(5%2);
+           
+        }
+    }
+}
