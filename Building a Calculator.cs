1f4525b2f89e886dca07e89489dddd55a0975f9d
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-14 21:17:54 +0530

    Fifth C# Code

---
+++ Building a Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second Number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            Console.ReadLine();
+        }
+    }
+}
