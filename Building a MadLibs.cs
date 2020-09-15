3e2328c8743a993def33f213aad3fdafdd0f02be
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-15 19:26:39 +0530

    Sixth C# Code

---
+++ Building a MadLibs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string colour, flowers, Celeberity;
+            Console.Write("Enter a colour");
+            colour = Console.ReadLine();
+            Console.Write("Enter a flowers");
+            flowers= Console.ReadLine();
+            Console.Write("Enter a Celeberity");
+            Celeberity = Console.ReadLine();
+
+
+
+            Console.WriteLine("Roses are "+ colour);
+            Console.WriteLine( flowers + " are blue");
+            Console.WriteLine("I love " + Celeberity);
+            Console.ReadLine();
+        }
+    }
+}
