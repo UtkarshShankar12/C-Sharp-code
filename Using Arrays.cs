5abd5b17c82e37937563f8ed93cf3cd6b4989d34
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-16 19:49:03 +0530

    Seventh C# Code

---
+++ Using Arrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckynumbers = { 1, 2, 3, 4, 5, 6 };
+            string[] freinds = new string[5]
+            Console.WriteLine(luckynumbers[2]);
+            Console.ReadLine();
+        }
+    }
+}
