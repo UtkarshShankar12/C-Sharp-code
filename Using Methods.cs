e0622d6b1427c7b93111f5672f826741a24a6a47
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-17 23:07:01 +0530

    Eight C# Code

---
+++ Using Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Harsh",20);
+            SayHi("Utkarsh",15);
+            SayHi("Tanya",18);
+            Console.ReadLine();
+        }
+
+        static void SayHi( string name , int age  )
+        {
+            Console.WriteLine("Hello " + name + " your age is " + age);
+        }
+    }
+}
