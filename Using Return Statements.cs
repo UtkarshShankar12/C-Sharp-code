0809f952ca0cae9175e47a154159d636173a1d06
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-18 21:29:40 +0530

    Ninth C# Code

---
+++ Using Return Statements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(cube (5));
+            Console.ReadLine();
+        }
+
+        static int cube(int num)
+        {
+            int results = num * num * num;
+            return results;
+        }
+    }
+}
