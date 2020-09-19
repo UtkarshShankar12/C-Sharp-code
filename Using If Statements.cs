9cbaa98dacf46c447257cc78a5b0ccf6160028a4
Utkarsh-Shankar-Coder <utkarshshankar1456@gmail.com>
2020-09-19 20:56:23 +0530

    Tenth C# Code

---
+++ Using If Statements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            if (isMale)
+            {
+                Console.WriteLine("You are a male");
+
+            }
+            else
+            {
+                Console.WriteLine("You are  a female");
+            }
+        }
+    }
+}
